da68e0798a0b34598028d77c089a662cd5769f7b
Smári McCarthy <smari@occrp.org>
2017-01-17 14:02:21 -0800

    Add tests for new election structure

--- voting/tests.py
+++ voting/tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from voting import apportion1d, dhondt_gen, sainte_lague_gen
+from voting import Rules, Election
 
 class TestMaxEntropy(unittest.TestCase):
 
@@ -17,6 +18,39 @@ class TestMaxEntropy(unittest.TestCase):
         prior_allocations = [1, 1]
         res = apportion1d(votes, num_seats, prior_allocations, sainte_lague_gen)
         self.assertEqual(res, [3, 2])
+
+class TestRules(unittest.TestCase):
+    def test_rules(self):
+        # TODO: Write test.
+        pass
+
+class TestBasicAllocation(unittest.TestCase):
+    def test_election_basic(self):
+        rules = Rules()
+        rules["constituency_seats"] = [1, 2]
+        rules["constituency_adjustment_seats"] = [2, 1]
+        rules["constituency_names"] = ["I", "II"]
+        rules["parties"] = ["A", "B"]
+
+        votes = [[501, 400], [301, 200]]
+        election = Election(rules, votes)
+        election.run()
+        self.assertEqual(election.results, [[2, 1], [2, 1]])
+
+    def test_election_basic2(self):
+        rules = Rules()
+        rules["constituency_seats"] = [1, 1]
+        rules["constituency_adjustment_seats"] = [1, 1]
+        rules["constituency_names"] = ["I", "II"]
+        rules["parties"] = ["A", "B", "C"]
+        rules["divisor"] = "dhondt"
+        rules["adjustment_threshold"] = 0.0
+        votes = [[4000, 2000, 3500], [3000, 1000, 2500]]
+
+        election = Election(rules, votes)
+        election.run()
+        self.assertEquals(election.results, [[1, 1, 0], [1, 0, 1]])
+        
 
 if __name__ == '__main__':
     unittest.main()
