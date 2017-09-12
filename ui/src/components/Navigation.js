ded8a88274b09bb4f44ae4700c356fa008e4dd68
Jón Levy <nonni@nonni.cc>
2017-09-12 22:43:57 +0000

    added Settings route and component

--- ui/src/components/Navigation.js
+++ ui/src/components/Navigation.js
@@ -10,12 +10,21 @@ import {
   NavLink
 } from 'reactstrap';
 
+import { NavLink as RRNavLink } from 'react-router-dom';
+
+import {Route} from 'react-router-dom';
+import Settings from './Settings';
+
+
 const Navigation = (props) => (
   <Navbar color="inverse" inverse toggleable>
     <NavbarToggler right onClick={this.toggle} />
     <NavbarBrand href="/">Votesim</NavbarBrand>
     <Collapse isOpen={props.isOpen} navbar>
       <Nav className="ml-auto" navbar>
+        <NavItem>
+          <NavLink to="/settings" component={Settings} activeClassName="active" tag={RRNavLink}>Settings</NavLink> 
+        </NavItem>      
         <NavItem>
           <NavLink href="https://github.com/smari/voting">Github</NavLink>
         </NavItem>
---
+++ ui/src/components/Settings.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+const Settings = (props) => (
+  <main>
+    Great stuff happening
+  </main>
+)
+
+export default Settings;
