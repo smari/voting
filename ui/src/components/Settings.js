72e26da0b5315a8a00dc8db6df274aa595f560ea
Jón Levy <nonni@nonni.cc>
2017-09-15 21:42:02 +0000

    pair defaultValue attr with election_rules props

--- ui/src/components/Settings.js
+++ ui/src/components/Settings.js
@@ -16,21 +16,18 @@ const Settings = (props) => {
   }  
   //const adjustmentThreshold = 100 * props.data.election_rules.adjustment_threshold;
   const primaryDividerRule = Object.keys(props.dividerRules).map((k, i) => {
-    const selected = ((k === props.electionRules.primary_divider) ? 'selected' : '')
     return (
-      <option key={i} selected={true} value={k}>{props.dividerRules[k]}</option>
+      <option key={i} value={k}>{props.dividerRules[k]}</option>
     )
   })
   const adjustmentDividerRule = Object.keys(props.dividerRules).map((k, i) => {
-    const selected = ((k === props.electionRules.adjustment_divider) ? 'selected' : '')
     return (
-      <option key={i} selected value={k}>{props.dividerRules[k]}</option>
+      <option key={i} value={k}>{props.dividerRules[k]}</option>
     )
   })
   const adjustmentMethods = Object.keys(props.adjustmentMethods).map((k, i) => {
-    const selected = ((k === props.electionRules.adjustment_method) ? 'selected' : '')
     return (
-      <option key={i} selected value={k}>{props.adjustmentMethods[k]}</option>
+      <option key={i} value={k}>{props.adjustmentMethods[k]}</option>
     )
   })
   return (
@@ -39,20 +36,20 @@ const Settings = (props) => {
         <h1>Settings</h1>
         <Form>
         <FormGroup>
-          <Label for="exampleSelect">Adjustment methods</Label>
-          <Input type="select" name="select" id="exampleSelect">
+          <Label for="exampleSelect">Primary divider rule</Label>
+          <Input type="select" defaultValue={props.electionRules.primary_divider} name="select" id="exampleSelect">
             {primaryDividerRule}
           </Input>
         </FormGroup>
         <FormGroup>
-          <Label for="exampleSelect">Primary divider rule</Label>
-          <Input type="select" name="select" id="exampleSelect">
+          <Label for="exampleSelect">Adjustment divider rule</Label>
+          <Input type="select" defaultValue={props.electionRules.adjustment_divider} name="select" id="exampleSelect">
             {adjustmentDividerRule}
           </Input>
         </FormGroup>
         <FormGroup>
           <Label for="exampleSelect">Adjustment method</Label>
-          <Input type="select" name="select" id="exampleSelect">
+          <Input defaultValue={props.electionRules.adjustment_method} type="select" name="select" id="exampleSelect">
             {adjustmentMethods}
           </Input>
         </FormGroup>
