184d9ffb0156749f59d7081ac00d59ef493e4310
marthagb <marthab16ru.is>
2018-06-04 10:59:50 +0000

    apportion in simulation

--- backend/simulate.py
+++ backend/simulate.py
@@ -3,6 +3,9 @@ from rules import Rules
 from math import sqrt
 from random import betavariate
 
+import voting
+
+
 def avg(v):
     return sum(v)/len(v)
 
@@ -25,9 +28,12 @@ def beta_distribution(m_ref_votes, var_param):
         m_shares.append([])
         for j in range(len(m_ref_votes[i])):
             mean_beta_distr = m_ref_votes[i][j]/ref_totals[i]
-            var_beta_distr = var_param*mean_beta_distr*(mean_beta_distr-1)
-            alpha, beta = beta_params(mean_beta_distr, var_param)
-            share = betavariate(alpha, beta)
+            if mean_beta_distr > 0:
+                var_beta = var_param*mean_beta_distr*(1-mean_beta_distr)
+                alpha, beta = beta_params(mean_beta_distr, var_param)
+                share = betavariate(alpha, beta)
+            else:
+                share = 0
             m_shares[i].append(share)
             m_votes[i].append(int(share*ref_totals[i]))
 
@@ -41,29 +47,6 @@ GENERATING_METHODS = {
     "beta": beta_gen
 }
 
-"""
-def testsim(m_ref_votes, n=10000, rho=0.1):
-    error = 0
-    resultset = []
-    print("Generating %d results" % n)
-    for it in range(n):
-        _, shares = beta_distribution(m_ref_votes, rho)
-        resultset.append(shares)
-
-    print("Generation done.")
-
-    for i in range(len(shares)):
-        for j in range(len(shares[i])):
-            sharesij = [resultset[k][i][j] for k in range(n)]
-            mbar = avg(sharesij)
-            variance = var(sharesij, mbar)
-            rhobar = variance/(mbar*(1-mbar))
-            error += (rho**2 - rhobar)**2
-    print("Error estimation done.")
-
-    return error
-"""
-
 def error(avg, ref):
     num_consts = len(avg)
     num_parties = len(avg[0])
@@ -73,44 +56,8 @@ def error(avg, ref):
             if type(ref) is list:
                 s += abs(avg[i][j] - ref[i][j])
             else:
-                s += abs(avg[i][j] -ref)
+                s += abs(avg[i][j] - ref)
     return s/(num_consts*num_parties)
-
-def sim(election, method, n=10000, var_param=0.1):
-    ref_votes = election.m_votes
-    ref_shares = [[c[j]/sum(c) for j in range(len(c))] for c in ref_votes]
-    simul_votes = []
-    simul_shares = []
-    gen = GENERATING_METHODS[method]
-    for k in range(n):
-        votes, shares = next(gen(ref_votes, var_param))
-        simul_votes.append(votes)
-        simul_shares.append(shares)
-
-    avg_simul_shares = []
-    var_simul_shares = []
-    avg_var_params = []
-
-    for i in range(len(simul_shares[0])):
-        avg_simul_shares.append([])
-        var_simul_shares.append([])
-        avg_var_params.append([])
-        for j in range(len(simul_shares[0][i])):
-            shares_ij = [simul_shares[k][i][j] for k in range(n)]
-            average = avg(shares_ij)
-            variance = var(shares_ij, average)
-            avg_simul_shares[i].append(average)
-            var_simul_shares[i].append(variance)
-            avg_var_params[i].append(variance/(average*(1-average)))
-
-    error_avg_simul_shares = error(avg_simul_shares, ref_shares)
-    #error_var_simul_shares = error(var_simul_shares, var_beta_distr)
-    error_avg_var_param = error(avg_var_params, var_param)
-
-    return simul_votes, simul_shares
-
-
-
 
 
 class SimulationRules(Rules):
@@ -118,7 +65,7 @@ class SimulationRules(Rules):
         super(SimulationRules, self).__init__()
         # Simulation rules
         self["simulate"] = False
-        self["simulation_count"] = 100
+        self["simulation_count"] = 10000
         self["simulation_variate"] = "beta"
 
 
@@ -129,8 +76,9 @@ class Simulation:
         self.ref_votes = election.m_votes
         self.ref_shares = [[v/sum(c) for v in c] for c in self.ref_votes]
         self.variate = self.rules["simulation_variate"]
-        # self.election = election
+        self.election = election
         self.var_param = var_param
+        self.results = []
 
     def gen_votes(self):
         self.simul_votes = []
@@ -145,49 +93,40 @@ class Simulation:
     def test_generated(self):
         avg_simul_shares = []
         var_simul_shares = []
-        avg_var_params = []
         var_beta_distr = []
 
         for i in range(len(self.simul_shares[0])):
             avg_simul_shares.append([])
             var_simul_shares.append([])
-            avg_var_params.append([])
             var_beta_distr.append([])
             for j in range(len(self.simul_shares[0][i])):
-                shares_ij = [self.simul_shares[k][i][j] for k in range(self.rules["simulation_count"])]
+                shares_ij = [self.simul_shares[k][i][j] 
+                            for k in range(self.rules["simulation_count"])]
                 average = avg(shares_ij)
                 variance = var(shares_ij, average)
                 avg_simul_shares[i].append(average)
                 var_simul_shares[i].append(variance)
-                avg_var_params[i].append(variance/(average*(1-average)))
 
-                var_beta_distr[i].append(self.var_param*self.ref_shares[i][j]*(self.ref_shares[i][j]-1))
+                var_beta_distr[i].append(self.var_param
+                                        *self.ref_shares[i][j]
+                                        *(self.ref_shares[i][j]-1))
 
 
         self.avg_simul_shares = avg_simul_shares
         self.var_simul_shares = var_simul_shares
-        self.avg_var_params = avg_var_params
         self.error_avg_simul_shares = error(avg_simul_shares, self.ref_shares)
         self.error_var_simul_shares = error(var_simul_shares, var_beta_distr)
-        self.error_avg_var_param = error(avg_var_params, self.var_param)
-        print("Errors:")
-        print(self.error_avg_simul_shares)
-        print(self.error_var_simul_shares)
-        print(self.error_avg_var_param)
 
-    def simulate(self):
+    def simulate(self, election_rules):
         """Simulate many elections."""
 
         gen = self.gen_votes()
         for i in range(self.rules["simulation_count"]):
-            results = next(gen)
-            # for r in e_rules:
-            #   e = Election(r, results)
-            #   e.run()
-
-        print("Simulation done.")
-        self.test_generated()
-
+            votes, shares = next(gen)
+            self.results.append([])
+            for j in range(len(election_rules)):
+              election = voting.Election(election_rules[j], votes)
+              self.results[i].append(election.run())
 
     def fetch_results(self):
         pass
--- backend/tests/simulation.py
+++ backend/tests/simulation.py
@@ -1,8 +1,10 @@
 # coding:utf-8
 from unittest import TestCase
+from random import uniform
 
 import logging
 import simulate
+import voting
 import util
 
 
@@ -15,6 +17,39 @@ class SimulationTest(TestCase):
         # Generate a few vote sets
 
     def test_generate_votes_average(self):
-        pass
-        # sim = simulate.Simulation([], [])
+        s_rules = simulate.SimulationRules()
+        s_rules["simulation_count"] = 10000
+        e_rules = voting.ElectionRules()
+        e_rules["constituency_names"] = ["I", "II", "III"]
+        e_rules["parties"] = ["A", "B"]
+        votes = [[500, 300], [200, 400], [350, 450]]
+        election = voting.Election(e_rules, votes)
+        sim = simulate.Simulation(s_rules, election, 0.1)
+        gen = sim.gen_votes()
+        r = []
+        r_avg = []
+        r_var = []
+        for k in range(s_rules["simulation_count"]):
+        	r.append([])
+        	_, shares = next(gen)
+        	for i in range(len(votes)):
+        		r[k].append([])
+        		for j in range(len(votes[i])):
+        			r[k][i].append(uniform(0.0,1.0))
+        for i in range(len(votes)):
+        	r_avg.append([])
+        	r_var.append([])
+        	for j in range(len(votes[i])):
+        		r_ij = [r[k][i][j] for k in range(s_rules["simulation_count"])]
+        		average = simulate.avg(r_ij)
+        		r_avg[i].append(average)
+        		r_var[i].append(simulate.var(r_ij, average))
+
+        sim.test_generated()
+        r_avg_error = simulate.error(r_avg, 0.5)
+        r_var_error = simulate.error(r_var, 1/12.0)
+
+       self.assertLessEqual(r_avg_error, 0.01)
+
+
         # Verify that µ=0.5±2%
