90a14ea02eeae4a99e3ad8ab02e75235410b0701
Smári McCarthy <smari@smarimccarthy.is>
2018-07-20 13:22:09 +0000

    Add seat allocations

--- vue-frontend/static/js/app.js
+++ vue-frontend/static/js/app.js
@@ -2,6 +2,8 @@ Vue.component('voting-votematrix', {
   data: function () {
     return {
       constituencies: ["Norðvestur", "Norðaustur", "Suður", "Suðvestur", "Reykjavík Suður", "Reykjavík Norður"],
+      constituency_seats: [8, 8, 9, 11, 9, 9],
+      constituency_adjustment_seats: [1, 1, 1, 2, 2, 2],
       parties: ["B", "C", "D", "F", "M", "S", "P", "V"],
       votes: [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],],
     }
@@ -30,11 +32,18 @@ Vue.component('voting-votematrix', {
   },
   template:
     `
+<b-container fluid class="votematrix-container">
 <table class="votematrix">
   <tr class="parties">
     <th class="small-12 medium-1 topleft">
       <b-button size="sm" variant="outline-secondary" @click="addConstituency()">Add constituency</b-button>
       <b-button size="sm" variant="outline-secondary" @click="addParty()">Add party</b-button>
+    </th>
+    <th>
+      <abbr title="Constituency seats"># Cons.</abbr>
+    </th>
+    <th>
+      <abbr title="Adjustment seats"># Adj.</abbr>
     </th>
     <th v-for="(party, partyidx) in parties" class="small-12 medium-1 column partyname">
       <b-button size="sm" variant="link" @click="deleteParty(partyidx)">×</b-button>
@@ -46,6 +55,12 @@ Vue.component('voting-votematrix', {
         <b-button size="sm" variant="link" @click="deleteConstituency(conidx)">×</b-button>
         <input type="text" v-model="constituencies[conidx]"></input>
     </th>
+    <td class="small-12 medium-2 column partyvotes">
+      <input type="text" v-model.number="constituency_seats[conidx]"></input>
+    </td>
+    <td class="small-12 medium-2 column partyvotes">
+      <input type="text" v-model.number="constituency_adjustment_seats[conidx]"></input>
+    </td>
     <td v-for="(party, partyidx) in parties" class="small-12 medium-2 column partyvotes">
         <input type="text" v-model.number="votes[conidx][partyidx]">
         </input>
