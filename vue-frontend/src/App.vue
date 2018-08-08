b4981089881342477ff3233908b65574b34e71b2
Smári McCarthy <smari@smarimccarthy.is>
2018-08-08 16:06:32 +0000

    Adding intro text

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <b-navbar toggleable="md" type="dark" variant="info">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand href="#">Voting</b-navbar-brand>
+      <b-navbar-brand href="#/">Voting</b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav>
           <b-nav-item href="#/election">Single Election</b-nav-item>
