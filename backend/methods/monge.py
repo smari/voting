7716cbd941ff6a9c25a7129cc737c16076f97410
PeturOA <peturoa1@gmail.com>
2018-08-24 00:29:09 +0000

    cleanup

--- backend/methods/monge.py
+++ backend/methods/monge.py
@@ -1,24 +1,37 @@
 from copy import deepcopy
 
-def monge(m_votes, v_total_seats, v_party_seats,
-                            m_prior_allocations, divisor_gen, threshold=None,
-                            orig_votes=None, **kwargs):
+def monge(
+    m_votes,
+    v_total_seats,
+    v_party_seats,
+    m_prior_allocations,
+    divisor_gen,
+    threshold=None,
+    orig_votes=None,
+    **kwargs
+):
     """Apportion by Monge algorithm"""
-
     m_allocations = deepcopy(m_prior_allocations)
     total_seats = sum(v_total_seats)
     while sum([sum(x) for x in m_allocations]) < total_seats:
-        best = find_best_Monge_list(m_votes, m_allocations, v_total_seats, v_party_seats, divisor_gen)
+        best = find_best_Monge_list(
+            m_votes, m_allocations, v_total_seats, v_party_seats, divisor_gen
+        )
         if best == None:
             # if we did not find any list to allocate to now,
             # then we also won't on next iteration
             # throw some exception perhaps?
             return m_allocations, "Adjustment seat allocation incomplete."
         m_allocations[best["constituency"]][best["party"]] += 1
-
     return m_allocations, None
 
-def find_best_Monge_list(votes, allocations, total_seats, party_seats, divisor_gen):
+def find_best_Monge_list(
+    votes,
+    allocations,
+    total_seats,
+    party_seats,
+    divisor_gen
+):
     #calculate max_Monge_ratio
     no_constituencies = len(votes)
     no_parties = len(votes[0])
@@ -29,7 +42,9 @@ def find_best_Monge_list(votes, allocations, total_seats, party_seats, divisor_g
         for P in range(no_parties):
             if no_seats_left_for_party(P, party_seats, allocations):
                 continue
-            closest = find_closest_comparison(C, P, votes, allocations, divisor_gen)
+            closest = find_closest_comparison(
+                C, P, votes, allocations, divisor_gen
+            )
             if closest == None:
                 # do not append, ignore list if there is no valid comparison
                 continue
@@ -54,7 +69,15 @@ def no_seats_left_for_constituency(constituency, v_total_seats, m_allocations):
     seats_left = v_total_seats[constituency] - sum(m_allocations[constituency])
     return seats_left <= 0
 
-def find_closest_comparison(C1, P1, votes, allocations, total_seats, party_seats, divisor_gen):
+def find_closest_comparison(
+    C1,
+    P1,
+    votes,
+    allocations,
+    total_seats,
+    party_seats,
+    divisor_gen
+):
     no_constituencies = len(votes)
     no_parties = len(votes[0])
     a = divided_vote(votes, allocations, C1, P1, divisor_gen)
