1b0b0d043607c3109c989c9817f4d78a714240b8
PeturOA <peturoa1@gmail.com>
2018-09-10 15:42:51 +0000

    simplify

--- backend/util.py
+++ backend/util.py
@@ -354,13 +354,8 @@ def election_to_xlsx(election, filename):
     worksheet.write(startrow+1, startcol, 'Constituency', cell_format)
     worksheet.write_row(startrow+1, startcol+1, parties, cell_format)
     worksheet.write_column(startrow+2, startcol, const_names, cell_format)
-    srow = startrow+2
-    scol = startcol+1
-    write_matrix(worksheet, srow, scol, xtd_const_seats, cell_format)
-    row = startrow+1
-    for c in range(len(xtd_const_seats)):
-        row += 1
-    row += 2
+    write_matrix(worksheet, startrow+2, startcol+1, xtd_const_seats, cell_format)
+    row = startrow + 3 + len(xtd_const_seats)
     worksheet.merge_range(row, 2, row, 6, "Adjustment seat apportionment",
                                 h_format)
     worksheet.merge_range(row, 7, row, 8, "Threshold", h_format)
