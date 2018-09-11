cd402c4e7d336ab1f11164700efcc53645e51f16
PeturOA <peturoa1@gmail.com>
2018-09-11 18:56:55 +0000

    simplify

--- backend/util.py
+++ backend/util.py
@@ -383,9 +383,7 @@ def election_to_xlsx(election, filename):
     for p in range(len(v_final_votes)):
         if v_final_votes[p] != 0:
             worksheet.write(startrow+4, p+2, v_final_vote_shares[p], share_format)
-    v_const_seats = []
-    for p in range(len(v_final_votes)):
-        v_const_seats.append(election.v_const_seats_alloc[p])
+    v_const_seats = election.v_const_seats_alloc
     v_const_seats.append(sum(v_const_seats))
     worksheet.write(startrow+5, 1, 'Constituency seats', cell_format)
     for p in range(len(v_const_seats)):
