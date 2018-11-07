9ce74e550c0720477c5c9083581a96af5ba62087
PeturOA <peturoa1@gmail.com>
2018-11-07 10:51:07 +0000

    rephrase in terms of stability

--- backend/simulate.py
+++ backend/simulate.py
@@ -13,12 +13,14 @@ from datetime import datetime, timedelta
 
 def beta_params(mean, deviation_param):
     assert(0<deviation_param and deviation_param<1)
+    stability_parameter = 1.0/deviation_param**2
+    assert(1<stability_parameter)
     assert(0<mean and mean<1)
     #make sure alpha and beta >1 to ensure nice probability distribution
     lower_mean = mean if mean<=0.5 else 1-mean
     assert(0<lower_mean and lower_mean<=0.5)
     lifting_factor = 1 + 1.0/lower_mean
-    weight = lifting_factor/deviation_param**2 - 1
+    weight = lifting_factor*stability_parameter - 1
     alpha = weight*mean
     beta = weight*(1-mean)
     assert(alpha>1)
