8a4b6af1f3efd264ed1c7bca96e5f78b3b64dbd6
PeturOA <peturoa1@gmail.com>
2018-11-07 01:34:31 +0000

    disallow beta parameter = 1

--- backend/simulate.py
+++ backend/simulate.py
@@ -12,17 +12,17 @@ from datetime import datetime, timedelta
 
 
 def beta_params(mean, deviation_param):
-    assert(0<deviation_param and deviation_param<=1)
+    assert(0<deviation_param and deviation_param<1)
     assert(0<mean and mean<1)
-    #TODO: make sure alpha and beta >=1 to ensure nice probability distribution
+    #TODO: make sure alpha and beta >1 to ensure nice probability distribution
     lower_mean = mean if mean<=0.5 else 1-mean
     assert(0<lower_mean and lower_mean<=0.5)
     lifting_factor = 1 + 1.0/lower_mean
     weight = lifting_factor/deviation_param**2 - 1
     alpha = weight*mean
     beta = weight*(1-mean)
-    assert(alpha>=1)
-    assert(beta>=1)
+    assert(alpha>1)
+    assert(beta>1)
     return alpha, beta
 
 def beta_distribution(
