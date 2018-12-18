79357b3e4777f679b4e902f806af3a7939766663
POA <peturoa1@gmail.com>
2018-12-18 15:27:18 +0000

    test different combinations

--- backend/tests/entropy.py
+++ backend/tests/entropy.py
@@ -25,6 +25,18 @@ class TestEntropy(unittest.TestCase):
     def test_entropy_depenency_on_divisor(self):
         dd_entropy = self.election.entropy()
         ds_entropy = entropy(self.votes, self.election.results, sainte_lague_gen)
+        self.rules["primary_divider"] = "sainte-lague"
+        self.rules["adj_determine_divider"] = "sainte-lague"
+        self.rules["adj_alloc_divider"] = "sainte-lague"
+        self.sl_election = Election(self.rules, self.votes)
+        self.sl_election.run()
+        ss_entropy = self.sl_election.entropy()
+        sd_entropy = entropy(self.votes, self.sl_election.results, dhondt_gen)
+        self.assertNotEqual(ds_entropy, sd_entropy)
         self.assertNotEqual(ds_entropy, dd_entropy)
+        self.assertNotEqual(ss_entropy, dd_entropy)
+        self.assertNotEqual(ss_entropy, sd_entropy)
+        self.assertEqual(round(ss_entropy, 2), 41.22)
+        self.assertEqual(round(sd_entropy, 2), 42.95)
         self.assertEqual(round(ds_entropy, 2), 41.22)
         self.assertEqual(round(dd_entropy, 2), 42.95)
