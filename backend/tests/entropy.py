5e4f5c5fb5318516a8fdd1bd3cf8d5e617b34d1d
POA <peturoa1@gmail.com>
2018-12-18 12:34:03 +0000

    simplify test

--- backend/tests/entropy.py
+++ backend/tests/entropy.py
@@ -23,10 +23,7 @@ class TestEntropy(unittest.TestCase):
         self.assertEqual(round(self.election.entropy(), 2), 42.95)
 
     def test_entropy_depenency_on_divisor(self):
-        sl_rules = ElectionRules()
-        sl_rules["adj_alloc_divider"] = "sainte-lague"
-        sl = sl_rules.get_generator("adj_alloc_divider")
-        sl_entropy = entropy(self.votes, self.election.results, sl)
+        sl_entropy = entropy(self.votes, self.election.results, sainte_lague_gen)
         dh_entropy = self.election.entropy()
         self.assertNotEqual(sl_entropy, dh_entropy)
         self.assertEqual(round(sl_entropy, 2), 41.22)
