09b2b8136f47401ebf81a439f947dd81ed92000c
POA <peturoa1@gmail.com>
2019-01-09 09:54:07 +0000

    add test for all_adj measure

---
+++ backend/tests/measures.py
@@ -0,0 +1,55 @@
+# coding:utf-8
+from unittest import TestCase
+from random import uniform
+
+import logging
+import simulate
+import voting
+import util
+
+
+class MeasureTest(TestCase):
+    def setUp(self):
+        self.e_rules = voting.ElectionRules()
+        self.e_rules["constituency_names"]            = ["I", "II", "III"]
+        self.e_rules["constituency_seats"]            = [ 1,    1,     1 ]
+        self.e_rules["constituency_adjustment_seats"] = [ 0,    0,     0 ]
+        self.e_rules["parties"] = ["A", "B"]
+        self.votes =             [[500, 600],
+                                  [200, 400],
+                                  [350, 450]]
+        self.s_rules = simulate.SimulationRules()
+        self.s_rules["simulation_count"] = 0
+
+    def test_all_adj(self):
+        #Arrange
+        election = voting.Election(self.e_rules, self.votes)
+        comparison_rules = simulate.generate_all_adj_ruleset(self.e_rules)
+        v_votes = [sum(x) for x in zip(*self.votes)]
+        comparison_election = voting.Election(comparison_rules, [v_votes])
+        sim = simulate.Simulation(self.s_rules, [self.e_rules], self.votes)
+
+        #Act
+        base_results = election.run()
+        comparison_results = comparison_election.run()
+        sim.simulate()
+        sim_result = sim.get_results_dict()
+
+        #Assert
+        list_measures = sim_result['data'][0]['list_measures']
+        self.assertEqual(list_measures["total_seats"]['avg'],
+                         util.add_totals(base_results))
+
+        self.assertEqual(base_results,       [[0, 1],
+                                              [0, 1],
+                                              [0, 1]])
+        base_totals = [sum(x) for x in zip(*base_results)]
+        self.assertEqual(base_totals,         [0, 3])
+        self.assertEqual(comparison_results, [[1, 2]])
+        deviation = simulate.dev([base_totals], comparison_results)
+        self.assertEqual(deviation, 2)
+
+        measures = sim_result['data'][0]['measures']
+        self.assertEqual(measures['dev_all_adj']['avg'], deviation)
+        self.assertEqual(measures['dev_all_adj']['var'], 0)
+        self.assertEqual(measures['dev_all_adj']['cnt'], 1)
