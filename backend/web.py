bde578be20916be039251844697d6f90272fb330
POA <peturoa1@gmail.com>
2019-06-26 11:55:18 +0000

    make error messages more precise

--- backend/web.py
+++ backend/web.py
@@ -95,10 +95,10 @@ def check_vote_table(vote_table):
     num_constituencies = len(vote_table["constituencies"])
 
     if not len(vote_table["votes"]) == num_constituencies:
-        raise ValueError("The data is malformed.")
+        raise ValueError("The vote_table does not match the constituency list.")
     for row in vote_table["votes"]:
         if not len(row) == num_parties:
-            raise ValueError("The data is malformed.")
+            raise ValueError("The vote_table does not match the party list.")
         for p in range(len(row)):
             if not row[p]: row[p] = 0
             if type(row[p]) != int: raise TypeError("Votes must be numbers.")
@@ -106,15 +106,17 @@ def check_vote_table(vote_table):
     for const in vote_table["constituencies"]:
         if "name" not in const or not const["name"]:
             raise KeyError(f"Missing data ('vote_table.constituencies[x].name')")
+            name = const["name"]
         for info in ["num_const_seats", "num_adj_seats"]:
             if info not in const:
-                raise KeyError(f"Missing data ('vote_table.constituencies[x].{info}')")
+                raise KeyError(f"Missing data ('{info}' for {name})")
             if not const[info]: const[info] = 0
             if type(const[info]) != int:
                 raise TypeError("Seat specifications must be numbers.")
         if const["num_const_seats"]+const["num_adj_seats"] <= 0:
             raise ValueError("Constituency seats and adjustment seats "
-                             "must add to a nonzero number.")
+                             "must add to a nonzero number. "
+                             f"This is not the case for {name}.")
 
     return vote_table
 
