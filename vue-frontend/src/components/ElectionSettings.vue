b32f0cdd4ca9c61e3829338c843b13958c1562ec
POA <peturoa1@gmail.com>
2019-06-29 16:30:41 +0000

    change layout of electoral system settings

--- vue-frontend/src/components/ElectionSettings.vue
+++ vue-frontend/src/components/ElectionSettings.vue
@@ -11,74 +11,70 @@
       </b-col>
     </b-row>
     <b-row>
-      <legend>Allocation of constituency seats</legend>
       <b-col>
-        <b-form-group
-          label="Threshold"
-          description="What threshold are parties required to reach within a particular constituency to qualify for constituency seats?"
-        >
-          <b-input-group append="%">
-            <b-form-input type="number" min="0" max="100"
-              v-model.number="rules.constituency_threshold"/>
-          </b-input-group>
-        </b-form-group>
+        <fieldset>
+          <legend>Allocation of constituency seats</legend>
+          <b-form-group
+            label="Rule"
+            description="Which rule should be used to allocate constituency seats to lists within each constituency?"
+          >
+            <b-form-select class="mb-3"
+              v-model="rules.primary_divider"
+              :options="capabilities.divider_rules"/>
+          </b-form-group>
+          <b-form-group
+            label="Threshold"
+            description="What threshold are parties required to reach within a particular constituency to qualify for constituency seats?"
+          >
+            <b-input-group append="%">
+              <b-form-input type="number" min="0" max="100"
+                v-model.number="rules.constituency_threshold"/>
+            </b-input-group>
+          </b-form-group>
+        </fieldset>
       </b-col>
       <b-col>
-        <b-form-group
-          label="Rule"
-          description="Which rule should be used to allocate constituency seats to lists within each constituency?"
-        >
-          <b-form-select class="mb-3"
-            v-model="rules.primary_divider"
-            :options="capabilities.divider_rules"/>
-        </b-form-group>
-      </b-col>
-    </b-row>
-    <b-row>
-      <legend>Division of adjustment seats</legend>
-      <b-col>
-        <b-form-group
-          label="Threshold"
-          description="What threshold are parties required to reach nationally to qualify for adjustment seats?"
-        >
-          <b-input-group append="%">
-            <b-form-input type="number" min="0" max="100"
-              v-model.number="rules.adjustment_threshold"/>
-          </b-input-group>
-        </b-form-group>
+        <fieldset>
+          <legend>Division of adjustment seats</legend>
+          <b-form-group
+            label="Rule"
+            description="Which rule should be used to divide adjustment seats between parties nationally?"
+          >
+            <b-form-select class="mb-3"
+              v-model="rules.adj_determine_divider"
+              :options="capabilities.divider_rules"/>
+          </b-form-group>
+          <b-form-group
+            label="Threshold"
+            description="What threshold are parties required to reach nationally to qualify for adjustment seats?"
+          >
+            <b-input-group append="%">
+              <b-form-input type="number" min="0" max="100"
+                v-model.number="rules.adjustment_threshold"/>
+            </b-input-group>
+          </b-form-group>
+        </fieldset>
       </b-col>
       <b-col>
-        <b-form-group
-          label="Rule"
-          description="Which rule should be used to divide adjustment seats between parties nationally?"
-        >
-          <b-form-select class="mb-3"
-            v-model="rules.adj_determine_divider"
-            :options="capabilities.divider_rules"/>
-        </b-form-group>
-      </b-col>
-    </b-row>
-    <b-row>
-      <legend>Allocation of adjustment seats</legend>
-      <b-col>
-        <b-form-group
-          label="Method"
-          description="Which method should be used to allocate adjustment seats?"
-        >
-          <b-form-select class="mb-3"
-            v-model="rules.adjustment_method"
-            :options="capabilities.adjustment_methods"/>
-        </b-form-group>
-      </b-col>
-      <b-col>
-        <b-form-group
-          label="Rule"
-          description="Which rule should be used to allocate adjustment seats to individual lists?"
-        >
-          <b-form-select class="mb-3"
-            v-model="rules.adj_alloc_divider"
-            :options="capabilities.divider_rules"/>
-        </b-form-group>
+        <fieldset>
+          <legend>Allocation of adjustment seats</legend>
+          <b-form-group
+            label="Rule"
+            description="Which rule should be used to allocate adjustment seats to individual lists?"
+          >
+            <b-form-select class="mb-3"
+              v-model="rules.adj_alloc_divider"
+              :options="capabilities.divider_rules"/>
+          </b-form-group>
+          <b-form-group
+            label="Method"
+            description="Which method should be used to allocate adjustment seats?"
+          >
+            <b-form-select class="mb-3"
+              v-model="rules.adjustment_method"
+              :options="capabilities.adjustment_methods"/>
+          </b-form-group>
+        </fieldset>
       </b-col>
     </b-row>
   </b-form>
