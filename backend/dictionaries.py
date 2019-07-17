9ff5d9b126ccf134f92ef1fb98ddc5689777d92b
POA <peturoa1@gmail.com>
2019-07-17 15:24:36 +0000

    rephrase quality measures

--- backend/dictionaries.py
+++ backend/dictionaries.py
@@ -82,10 +82,10 @@ MEASURES = {
     "dev_one_const":   "Allocation as if all constituencies were combined into one",
     "entropy":         "Entropy (product of all seat values used)",
     "entropy_ratio":   "Relative entropy deviation from optimal solution",
-    "loosemore_hanby": "Proportionality index according to Loosemore-Hanby (using the chosen fair shares)",
-    "sainte_lague":    "Scaled sum of squared deviation of list seats from fair seat shares (Sainte-Lague)",
+    "loosemore_hanby": "Proportionality index according to Loosemore-Hanby",
+    "sainte_lague":    "Scaled sum of squared deviation of list seats from ideal seat shares (Sainte-Lague)",
     "dhondt_min":      "Mininum seat value used (d'Hondt)",
-    "dhondt_sum":      "Scaled sum of positive deviation of list seats from fair seat shares (d'Hondt)",
+    "dhondt_sum":      "Scaled sum of positive deviation of list seats from ideal seat shares (d'Hondt)",
 }
 DEVIATION_MEASURES = [
     "dev_opt",
