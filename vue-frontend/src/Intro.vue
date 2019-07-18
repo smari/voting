2f2421bbc5a22ab94fcad1204e010a8e1919cb2a
POA <peturoa1@gmail.com>
2019-07-18 17:11:19 +0000

    stop wasting so much space on the website

--- vue-frontend/src/Intro.vue
+++ vue-frontend/src/Intro.vue
@@ -3,7 +3,7 @@
   <h1>Voting system simulator</h1>
 
   <span>
-    Version: 1.1.13 Time of release: 2019.07.18-14:48 (GMT)
+    Version: 1.1.14 Time of release: 2019.07.18-17:12 (GMT)
   </span>
 
   <p>
