fa6f627e374a851376b7f8b09ccc9dd172a011a3
POA <peturoa1@gmail.com>
2019-07-20 14:12:47 +0000

    display step-by-step demonstration in webpage also

---
+++ vue-frontend/src/components/ResultDemonstration.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container fluid>
+    <h3>Step-by-step demonstration of adjustment seat allocations</h3>
+    <table class="demomatrix" v-if="table.steps.length > 0">
+      <tr>
+        <th v-for="header in table.headers">
+          {{ header }}
+        </th>
+      </tr>
+      <tr v-for="(step, stepidx) in table.steps">
+        <template v-for="(col, colidx) in step">
+          <td>
+            {{ col }}
+          </td>
+        </template>
+      </tr>
+    </table>
+  </b-container>
+</template>
+<script>
+export default {
+  props: {
+    "table": { default: { "headers": [], "steps": [] } },
+  }
+}
+</script>
